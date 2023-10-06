f75c553e17f5e2759bc2d54364ac367df07824fe
kadirfurkanguler <kadirfurkanguler@gmail.com>
2023-10-06 00:25:55 -0600

    Toast Package Added

---
+++ android/app/src/main/java/com/rnandroidnative/ToastModule.java
@@ -0,0 +1,22 @@
+package com.rnandroidnative;
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ToastModule";
+    }
+
+    @ReactMethod
+    public void showToast(String message) {
+        Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
